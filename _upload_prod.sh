40facb8558c7ae63430c42228f1c436da7db0e8e
Joel Verhagen <joel.verhagen@gmail.com>
2014-04-07 08:38:05 -0400

    Update template slightly Disable stage password Upload to new host

--- _upload_prod.sh
+++ _upload_prod.sh
@@ -5,6 +5,7 @@ jekyll build --config _config_prod.yml
 
 # upload
 rsync --verbose --compress --recursive --checksum --delete _prod/ joelverhagen@joelverhagen.com:sites/joelverhagen.com/
+rsync --verbose --compress --recursive --checksum --delete _prod/ joel@do.joelverhagen.com:/var/www/joelverhagen.com/
 
 # delete
 rm -rf _prod
--- _upload_stage.sh
+++ _upload_stage.sh
@@ -4,11 +4,11 @@ set -e
 jekyll build --config _config_stage.yml
 
 # add _stage_extra
-cp -f _stage_extra/.htaccess _stage/.htaccess
 cp -rf _stage_extra/* _stage/
 
 # upload
 rsync --verbose --compress --recursive --checksum --delete _stage/ joelverhagen@joelverhagen.com:sites/s.joelverhagen.com/
+rsync --verbose --compress --recursive --checksum --delete _stage/ joel@sdo.joelverhagen.com:/var/www/s.joelverhagen.com/
 
 # delete
 rm -rf _stage
