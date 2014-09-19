ede1d119358f7ba246c63f098fdc1b43f45bc8bf
Joel Verhagen <joel.verhagen@gmail.com>
2014-09-18 21:22:15 -0700

    Update resume and current occupation.

--- _upload_prod.sh
+++ _upload_prod.sh
@@ -3,9 +3,8 @@ set -e
 # build
 jekyll build --config _config_prod.yml
 
-# upload
-rsync --verbose --compress --recursive --checksum --delete _prod/ joelverhagen@joelverhagen.com:sites/joelverhagen.com/
-rsync --verbose --compress --recursive --checksum --delete _prod/ joel@do.joelverhagen.com:/var/www/joelverhagen.com/
+# copy
+rsync --verbose --compress --recursive --checksum --delete _prod/ /var/www/joelverhagen.com/
 
 # delete
 rm -rf _prod
--- _upload_stage.sh
+++ _upload_stage.sh
@@ -7,8 +7,7 @@ jekyll build --config _config_stage.yml
 cp -rf _stage_extra/* _stage/
 
 # upload
-rsync --verbose --compress --recursive --checksum --delete _stage/ joelverhagen@joelverhagen.com:sites/s.joelverhagen.com/
-rsync --verbose --compress --recursive --checksum --delete _stage/ joel@sdo.joelverhagen.com:/var/www/s.joelverhagen.com/
+rsync --verbose --compress --recursive --checksum --delete _stage/ /var/www/s.joelverhagen.com/
 
 # delete
 rm -rf _stage
